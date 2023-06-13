5f65ad11f8efe34e05c49f4d5a95384a5b16e090
Lorenzo de Souza <128727183+l0orenzz@users.noreply.github.com>
2023-06-13 11:32:44 -0300

    upload testing

---
+++ GitLearning/src/BotSucessorGit/Bot.java
@@ -0,0 +1,16 @@
+package BotSucessorGit;
+
+public class Bot{
+	
+	public static void main (String args[]) {
+		
+		int start = 0;
+		int end = 10;
+		
+		for(start = 0; start<end; start++) {
+			
+			System.out.printf("\nNumber %d", start);
+			
+		}
+	}
+}
---
+++ GitLearning/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lorenzokirstdesouza
+ *
+ */
+module GitLearning {
+}
